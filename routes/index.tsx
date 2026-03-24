702e3b87c4a9b49c79b78aa526db2260c3b07287
Martin Allsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2026-03-24 10:53:36 -0400

    Init

---
+++ routes/index.tsx
@@ -0,0 +1,21 @@
+import { Head } from "fresh/runtime";
+import { define } from "../utils.ts";
+
+export default define.page(function Home(_ctx) {
+
+
+  return (
+    <div class="">
+      <Head>
+        <title>Martin Allsbrook</title>
+        <meta 
+          name="description" 
+          content="Martin Allsbrook is a developer & 3d artist based in Brooklyn, NY."
+        />
+      </Head>
+      <div class="">
+        <h1>Welcome to Martin Allsbrook's Portfolio</h1>
+      </div>
+    </div>
+  );
+});
---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import { fresh } from "@fresh/plugin-vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [
+    fresh(), 
+    tailwindcss(),
+    cloudflare(),
+  ],
+});
