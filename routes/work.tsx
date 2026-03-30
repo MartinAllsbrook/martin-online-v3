5471c937d19e95e3b2a198c48939240ba5e9d207
Martin Allsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2026-03-30 11:58:28 -0400

    Update work page to use Parse Posts

--- routes/work.tsx
+++ routes/work.tsx
@@ -2,47 +2,52 @@ import { page, PageProps } from "fresh";
 import { define } from "../utils.ts";
 import PageWrap from "../components/PageWrap.tsx";
 import getEnvVar, { getPayloadBinding } from "../src/GetEnv.ts";
+import { Post } from "src/types/Post.ts";
+import { parsePostsResponse } from "src/ParsePost.ts";
 
-interface ExpectedData {
-    docs: {
-        id: number;
-        title: string;
-        slug: string;
-        featuredImage: {
-            url: string;
-            alt: string;
-        };
-    }[];
+interface ExpectedResponse {
+    posts: Post[];
+}
+
+interface Data {
+    posts: Post[];
+    payloadUrl: string;
 }
 
 
 // https://fresh.deno.dev/docs/concepts/data-fetching
 export const handler = define.handlers({
     async GET(_ctx) {
+
         const payloadUrl = await getEnvVar("PAYLOAD_URL");
         const binding = await getPayloadBinding();
+        
         console.log("Fetching posts from:", `${payloadUrl}/api/posts`);
+        
         const response = await (binding ?? globalThis).fetch(`${payloadUrl}/api/posts`);
+        
         if (!response.ok) {
             throw new Error(`CMS responded ${response.status}: ${await response.text()}`);
         }
-        const data: ExpectedData = await response.json();
-        return page(data);
+        
+        const json = await response.json();
+        const posts: Post[] = parsePostsResponse(json);
+        return page({ posts, payloadUrl });
     }
 });
 
-export default define.page(function Work({ data }: PageProps<ExpectedData>) {
+export default define.page(function Work({ data }: PageProps<Data>) {
     return (
         <PageWrap head={{
             title: "Work - Martin Allsbrook",
             description: "A selection of projects I've worked on, including games, web development, 3d art, and design."
         }} index={[]}>
             <h1>Work</h1>
-            {data?.docs.map((post) => (
+            {data?.posts.map((post) => (
                 <div key={post.id}>
                     <a href={`/work/${post.slug}`}>
                         <h2>{post.title}</h2>
-                        <img src={`http://localhost:3000${post.featuredImage.url}`} alt={post.featuredImage.alt} />
+                        <img src={`${data.payloadUrl}${post.featuredImage.url}`} alt={post.featuredImage.alt} />
                     </a>
                 </div>
             ))}
