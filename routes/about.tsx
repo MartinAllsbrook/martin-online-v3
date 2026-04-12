3b18a72bca7191175be52f47c86b2dca700ca726
MartinAllsbrook <martin.allsbrook@colorado.edu>
2026-04-12 19:09:43 -0400

    About using image

--- routes/about.tsx
+++ routes/about.tsx
@@ -2,6 +2,7 @@ import { page, PageProps } from "fresh";
 import { define } from "../utils.ts";
 import PageWrap from "../components/PageWrap.tsx";
 import { Image } from "src/types/Post.ts";
+import ImageComponent from "islands/Image.tsx";
 
 interface MediaData {
     image: Image;
@@ -22,13 +23,13 @@ export default define.page(function About({ data }: PageProps<MediaData>) {
     return (
         <div class="">
             <PageWrap head = {{ title: "About - Martin Allsbrook" }} index={[{ name: "back", href: "./" }]} route={[ { name: "about", href: "./about" } ]}>
-                <h1 class="mb-4">About Me</h1>
+                <h1 class="mb-4">About</h1>
                 <div class="flex flex-col sm:flex-row">
-                    <img class="hidden sm:block sm:w-1/3 mr-4" src={`${data?.payloadUrl}${data?.image?.url}`} alt={data?.image?.alt} />
+                    <ImageComponent className="hidden sm:block sm:w-1/3 mr-4" src={`${data?.payloadUrl}${data?.image?.url}`} alt={data?.image?.alt} aspectRatio="1"/>
                     <p class="mb-4 sm:w-2/3">
                         I'm Martin Allsbrook, a Brooklyn-based developer and 3D artist with a passion for games, programming, and the web. My experiences span architectural and product renders to interactive web applications and games. I love the challenge of making things move and interact, and I'm always eager to learn new skills.
                     </p>
-                    <img class="sm:hidden" src={`${data?.payloadUrl}${data?.image?.url}`} alt={data?.image?.alt} />
+                    <ImageComponent className="sm:hidden" src={`${data?.payloadUrl}${data?.image?.url}`} alt={data?.image?.alt} aspectRatio="1"/>
                 </div>
             </PageWrap>
         </div>
