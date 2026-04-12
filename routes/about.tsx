f1f2207f492e72c256f48b27bdf9981db5dd76b2
MartinAllsbrook <martin.allsbrook@colorado.edu>
2026-04-12 19:06:12 -0400

    About page

--- routes/about.tsx
+++ routes/about.tsx
@@ -1,19 +1,20 @@
-import { PageProps } from "fresh";
+import { page, PageProps } from "fresh";
 import { define } from "../utils.ts";
 import PageWrap from "../components/PageWrap.tsx";
+import { Image } from "src/types/Post.ts";
 
 interface MediaData {
-    url: string;
-    alt: string;
+    image: Image;
+    payloadUrl: string;
 }
 
 export const handler = define.handlers({
     async GET(_ctx) {
         const payloadUrl = Deno.env.get("PAYLOAD_URL");
-        const response = await fetch(`${payloadUrl}/api/media/1`);
+        const response = await fetch(`${payloadUrl}/api/media?where[tags][in]=about&limit=1`);
         const data = await response.json();
         console.log("data", data);
-        return { data };
+        return page({ image: data?.docs?.[0], payloadUrl });
     }
 });
 
@@ -21,8 +22,14 @@ export default define.page(function About({ data }: PageProps<MediaData>) {
     return (
         <div class="">
             <PageWrap head = {{ title: "About - Martin Allsbrook" }} index={[{ name: "back", href: "./" }]} route={[ { name: "about", href: "./about" } ]}>
-                <h1>About</h1>
-                <img src={`http://localhost:3000${data?.url}`} alt={data?.alt} />
+                <h1 class="mb-4">About Me</h1>
+                <div class="flex flex-col sm:flex-row">
+                    <img class="hidden sm:block sm:w-1/3 mr-4" src={`${data?.payloadUrl}${data?.image?.url}`} alt={data?.image?.alt} />
+                    <p class="mb-4 sm:w-2/3">
+                        I'm Martin Allsbrook, a Brooklyn-based developer and 3D artist with a passion for games, programming, and the web. My experiences span architectural and product renders to interactive web applications and games. I love the challenge of making things move and interact, and I'm always eager to learn new skills.
+                    </p>
+                    <img class="sm:hidden" src={`${data?.payloadUrl}${data?.image?.url}`} alt={data?.image?.alt} />
+                </div>
             </PageWrap>
         </div>
     );
